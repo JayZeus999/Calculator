41d254d355d54b07967b665cdddf33c449c716bb
Falade Joseph <faladejoseph400@gmail.com>
2025-10-02 07:44:24 +0100

    Add buttons & display

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const add = (a,b) => 
     a + b;
 
-add();
+console.log(add());
 
 const subtract = (a,b) =>
     a - b;
@@ -17,3 +17,26 @@ const divide = (a,b) =>
     a / b;
 
 divide();
+
+
+let num1;
+let operator;
+let num2;
+
+const operate = function (num1, num2){
+    return multiply(num1,num2);
+}
+
+operate();
+
+
+const container = document.querySelector("#container");
+document.body.appendChild(container);
+
+let display = document.querySelector("#display");
+container.append(display);
+
+let displayPanel = function (){
+    let display = " ";
+    
+}
